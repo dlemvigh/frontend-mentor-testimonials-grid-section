64594af3dd3c4b3893ebcd8649a0738bdb154d26
David Lemvigh <david.lemvigh@awaze.com>
2025-01-28 16:14:09 +0100

    Quick svelte setup

---
+++ src/App.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+  import Testimonial from "./lib/Testimonial.svelte";
+</script>
+
+<main>
+  <Testimonial
+    colorScheme="purple"
+    wide={true}
+    avatar="images/image-daniel.jpg"
+    name="Daniel Clifford"
+    title="Verified Graduate"
+    heading="I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny’s worth."
+  >
+    “ I was an EMT for many years before I joined the bootcamp. I’ve been
+    looking to make a transition and have heard some people who had an amazing
+    experience here. I signed up for the free intro course and found it
+    incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the
+    best - and most grueling - time of my life. Since completing the course,
+    I’ve successfully switched careers, working as a Software Engineer at a VR
+    startup. ”
+  </Testimonial>
+
+  <Testimonial
+    colorScheme="grey"
+    avatar="images/image-jonathan.jpg"
+    name="Jonathan Walters"
+    title="Verified Graduate"
+    heading="The team was very supportive and kept me motivated"
+  >
+    “ I started as a total newbie with virtually no coding skills. I now work as
+    a mobile engineer for a big company. This was one of the best investments
+    I’ve made in myself. ”
+  </Testimonial>
+
+  <Testimonial
+    tall={true}
+    colorScheme="white"
+    avatar="images/image-kira.jpg"
+    name="Kira Whittle"
+    title="Verified Graduate"
+    heading="Such a life-changing experience. Highly recommended!"
+  >
+    “ Before joining the bootcamp, I’ve never written a line of code. I needed
+    some structure from professionals who can help me learn programming step by
+    step. I was encouraged to enroll by a former student of theirs who can only
+    say wonderful things about the program. The entire curriculum and staff did
+    not disappoint. They were very hands-on and I never had to wait long for
+    assistance. The agile team project, in particular, was outstanding. It took
+    my learning to the next level in a way that no tutorial could ever have. In
+    fact, I’ve often referred to it during interviews as an example of my
+    developent experience. It certainly helped me land a job as a full-stack
+    developer after receiving multiple offers. 100% recommend! ”
+  </Testimonial>
+
+  <Testimonial
+    colorScheme="white"
+    avatar="images/image-jeanette.jpg"
+    name="Jeanette Harmon"
+    title="Verified Graduate"
+    heading="An overall wonderful and rewarding experience"
+  >
+    “ Thank you for the wonderful experience! I now have a job I really enjoy,
+    and make a good living while doing something I love. ”
+  </Testimonial>
+
+  <Testimonial
+    colorScheme="black"
+    wide={true}
+    avatar="images/image-patrick.jpg"
+    name="Patrick Abrams"
+    title="Verified Graduate"
+    heading="Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and 
+  learning from their experiences was easy."
+  >
+    “ The staff seem genuinely concerned about my progress which I find really
+    refreshing. The program gave me the confidence necessary to be able to go
+    out in the world and present myself as a capable junior developer. The
+    standard is above the rest. You will get the personal attention you need
+    from an incredible community of smart and amazing people. ”
+  </Testimonial>
+</main>
+
+<style>
+  main {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    gap: 1rem;
+    max-width: 1440px;
+    margin: 0 auto;
+    padding: 2rem;
+  }
+</style>
---
+++ src/lib/Testimonial.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+  export let colorScheme: "purple" | "grey" | "white" | "black";
+  export let wide: boolean;
+  export let tall: boolean;
+  export let avatar: string;
+  export let name: string;
+  export let title: string;
+  export let heading: string;
+</script>
+
+<article class="card color-{colorScheme}" class:wide class:tall>
+  <div class="author">
+    <img class="author-avatar" src={avatar} alt={name} />
+    <div class="author-name">{name}</div>
+    <div class="author-title">{title}</div>
+  </div>
+  <strong class="heading">{heading}</strong>
+  <p class="content">
+    <slot />
+  </p>
+</article>
+
+<style>
+  .card {
+    border-radius: 0.5rem;
+    overflow: hidden;
+    padding: 1rem;
+
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+  .author {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-auto-flow: row dense;
+    row-gap: 0.5rem;
+    column-gap: 1rem;
+  }
+  .author-avatar {
+    grid-row: span 2;
+    border-radius: 50%;
+    border: 2px solid hotpink;
+    height: 3rem;
+    width: 3rem;
+  }
+  .content {
+    margin: 0;
+  }
+  .color-purple {
+    background-color: rebeccapurple;
+    color: white;
+  }
+  .color-grey {
+    background-color: grey;
+    color: white;
+  }
+  .color-white {
+    background-color: white;
+    color: #333;
+  }
+  .color-black {
+    background-color: black;
+    color: white;
+  }
+
+  @media (min-width: 690px) {
+    .wide {
+      grid-column: span 2;
+    }
+    .tall {
+      grid-row: span 2;
+    }
+  }
+</style>
